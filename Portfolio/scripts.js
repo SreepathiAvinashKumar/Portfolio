55ff2ba3318d20ce5f9a9553546c2cbb5740d2e6
Sripathi Avinash Kumar <65992275+SreepathiAvinashKumar@users.noreply.github.com>
2023-06-27 22:40:22 +0530

    added script for hamburger

--- Portfolio/scripts.js
+++ Portfolio/scripts.js
@@ -15,3 +15,21 @@ var services = document.getElementById("services");
 arrow.addEventListener("click",()=>{
     window.scrollBy(0,600);
 })
+
+//menu actions
+
+var menubar  = document.getElementById("menu-button");
+var menubarbar1 = document.getElementById("bar-1-1");
+var menubarbar2 = document.getElementById("bar-2-2");
+var menubarbar3 = document.getElementById("bar-3-3");
+var menulist = document.getElementById("menuList");
+
+
+
+menubar.addEventListener("click",()=>{
+   menubarbar1.classList.toggle("changebar-1");
+   menubarbar2.classList.toggle("changebar-2");
+   menubarbar3.classList.toggle("changebar-3");
+   menulist.classList.toggle("expand");
+   menubar.style.backgroundColor = "transparent";
+})
